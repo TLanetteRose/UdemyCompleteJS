5ea8d4c80fcbe7bea4390bfbe622b759c1e97af5
T. Lanette <TLPollard4516@gmail.com>
2019-03-02 17:34:26 -0500

    Updated Looping
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- JSLanguageBasics/script.js
+++ JSLanguageBasics/script.js
@@ -505,8 +505,39 @@ for (var i = 0; i < 10; i++) {
 // ...
 // i = 9, 9 < 10 is true, log to console, i++
 // i = 10, 10 < 10 is FALSE, exit loop
-
+/*
 var john = ['John', 'Smith', 1990, 'designer', false];
 for (var i = 0; i < john.length; i++) {
     console.log(john[i]);
 }
+
+//While Loop
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+//continue and break statements
+/*
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+//Loop backwards
+
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
+*/
+
+/****
+* Coding Challenge 5
+*/
